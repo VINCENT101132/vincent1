95d9f6763334521783a06ee2993f902add03cbdd
家佑 <86872089+VINCENT101132@users.noreply.github.com>
2021-07-10 09:39:44 +0000

    ==

---
+++ 20210710/class/2.py
@@ -0,0 +1,4 @@
+hi=float(input('身高(m):'))
+he=float(input('體重(kg):'))
+BMI=he/hi**2
+print('bmi={0:.2f}'format(BMI))
---
+++ 20210710/class/4.py
@@ -0,0 +1,6 @@
+password=input("密碼")
+if password=="0419":
+  print('歡迎光臨')
+else:
+  print('密碼錯誤')
+print('The end')
