e973bdefe2e89479b7723620821ebac7b596f6b2
家佑 <86872089+VINCENT101132@users.noreply.github.com>
2021-07-16 06:41:18 +0000

    final hw

--- 20210713/homework/3.py
+++ 20210713/homework/3.py
@@ -5,3 +5,12 @@ e.g.
 import time
 time.sleep(1)
 """
+
+import turtle
+import time
+for a in range(60):
+  turtle.forward(50)
+  turtle.home()
+  time.sleep(1)
+  turtle.clear()
+  turtle.right(6*(a+1))
