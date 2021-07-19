573ec8e72b7b08a30d1c523973309056e3fa820e
家佑 <86872089+VINCENT101132@users.noreply.github.com>
2021-07-19 08:14:50 +0000

    I'm so tired

--- 20210717/homework/2.py
+++ 20210717/homework/2.py
@@ -4,3 +4,17 @@
 ​
 ​
 """
+
+n=int(input("請輸入整數"))
+b=0
+if n>1:
+  print(2)
+  for i in range(3,n+1):
+    for j in range(2,i):
+      if i%j==0:
+        b=1
+        break
+      else:
+        b=0
+    if b==0:
+      print(i)
