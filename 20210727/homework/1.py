af17b33f521b1fc026fd8a1df5f5d1192ed9321a
家佑 <86872089+VINCENT101132@users.noreply.github.com>
2021-07-30 07:48:59 +0000

    hw

--- 20210727/homework/1.py
+++ 20210727/homework/1.py
@@ -3,3 +3,18 @@
 並使用function def 求最小公倍數
 value = lcm(num1, num2)
 '''
+
+def lcm(x, y):
+   if x > y:
+       largest = x
+   else:
+       largest = y
+   while(True):
+       if((largest % x == 0) and (largest % y == 0)):
+           lcm = largest
+           break
+       largest += 1
+   return lcm
+num1 = int(input("輸入整數"))
+num2 = int(input("輸入整數"))
+print(lcm(num1, num2))
